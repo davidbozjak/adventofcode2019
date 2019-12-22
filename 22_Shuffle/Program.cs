17f9160b70e7804c2ae1527659d4b8f4d100c9ef
David Božjak <bozjak@gmail.com>
2019-12-22 10:56:07 +0100

    Added part 2

--- 22_Shuffle/Program.cs
+++ 22_Shuffle/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace _22_Shuffle
 {
@@ -9,9 +10,15 @@ namespace _22_Shuffle
     {
         static void Main(string[] args)
         {
-            using var inputProvider = new InputProvider<IInstruction<int>>("Input.txt", GetInstruction);
+            using var intInputProvider = new InputProvider<IInstruction<int>>("Input.txt", GetInstruction);
 
-            Part1(10007, inputProvider);
+            Part1(10007, intInputProvider);
+
+            using var longInputProvider = new InputProvider<IInstruction<long>>("Input.txt", GetInstruction);
+            Part2(2020, 119315717514047, 101741582076661, longInputProvider.ToList());
+            
+            //testing with part1 input:
+            //Part2(2306, 10007, 1, longInputProvider.ToList());    // Must return 2019
         }
 
         private static void Part1(int deckSize, IEnumerator<IInstruction<int>> instructionSet)
@@ -23,16 +30,40 @@ namespace _22_Shuffle
                 deck = instructionSet.Current.ApplyTechnique(deck.ToList().AsReadOnly());
             }
 
-            //Console.WriteLine("Deck after all instructions");
-            //Console.WriteLine(string.Join(", ", deck));
-
             var indexToFind = 2019;
             var indexOfCard = deck.IndexOf(indexToFind);
             Console.WriteLine("Part 1 Done");
             Console.WriteLine($"Index of card {indexToFind}: {indexOfCard}");
         }
 
-        private static bool GetInstruction(string? input, out IInstruction<int> value)
+        private static void Part2(long indexToLookAt, long deckSize, long repeatCount, IList<IInstruction<long>> instructionSet)
+        {
+            BigInteger Y = IndexAfterInstructionSet(indexToLookAt);
+            BigInteger Z = IndexAfterInstructionSet(Y);
+            BigInteger A = (Y - Z) * ModInverse(indexToLookAt - Y + deckSize, deckSize) % deckSize;
+            while (A < 0) A += deckSize;
+            BigInteger B = (Y - A * indexToLookAt) % deckSize;
+            while (B < 0) B += deckSize;
+
+            var cardAtIndexToLookAt = (BigInteger.ModPow(A, repeatCount, deckSize) * indexToLookAt + (BigInteger.ModPow(A, repeatCount, deckSize) - 1) * ModInverse(A - 1, deckSize) * B) % deckSize;
+
+            Console.WriteLine("Part 2 Done");
+            Console.WriteLine($"Card at index {indexToLookAt} is: {cardAtIndexToLookAt}");
+
+            BigInteger IndexAfterInstructionSet(BigInteger index)
+            {
+                var reversedOrderInstructions = instructionSet.Reverse();
+
+                foreach (var instruction in reversedOrderInstructions)
+                {
+                    index = instruction.IndexAfterTechniqueIsApplied(index, deckSize);
+                }
+
+                return index;
+            }
+        }
+
+        private static bool GetInstruction<T>(string? input, out IInstruction<T> value)
         {
             value = default;
 
@@ -43,19 +74,19 @@ namespace _22_Shuffle
             {
                 if (input.ToLower() == "deal into new stack")
                 {
-                    value = new DealNewStackInstruction<int>();
+                    value = new DealNewStackInstruction<T>();
                     return true;
                 }
                 else if (input.ToLower().StartsWith("deal with increment"))
                 {
                     int n = int.Parse(input.Substring("deal with increment".Length + 1));
-                    value = new DealIncrementN<int>(n);
+                    value = new DealIncrementN<T>(n);
                     return true;
                 }
                 else if (input.ToLower().StartsWith("cut"))
                 {
                     int n = int.Parse(input.Substring("cut".Length + 1));
-                    value = new CutNInstruction<int>(n);
+                    value = new CutNInstruction<T>(n);
                     return true;
                 }
             }
@@ -65,6 +96,23 @@ namespace _22_Shuffle
             }
 
             return false;
+        }
+
+        private static BigInteger ModInverse(BigInteger a, BigInteger n)
+        {
+            BigInteger i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                BigInteger t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
         }
 
         class DealIncrementN<T> : IInstruction<T>
@@ -87,6 +135,12 @@ namespace _22_Shuffle
 
                 return output.ToList();
             }
+
+            public BigInteger IndexAfterTechniqueIsApplied(BigInteger index, long deckSize)
+            {
+                var result = ModInverse(N, deckSize) * index % deckSize;
+                return result;
+            }
         }
 
         class CutNInstruction<T> : IInstruction<T>
@@ -108,6 +162,12 @@ namespace _22_Shuffle
 
                 return output;
             }
+
+            public BigInteger IndexAfterTechniqueIsApplied(BigInteger index, long deckSize)
+            {
+                var result = (index + N + deckSize) % deckSize;
+                return result;
+            }
         }
 
         class DealNewStackInstruction<T> : IInstruction<T>
@@ -122,11 +182,19 @@ namespace _22_Shuffle
                 output.Reverse();
                 return output;
             }
+
+            public BigInteger IndexAfterTechniqueIsApplied(BigInteger index, long deckSize)
+            {
+                var result = deckSize - index - 1;
+                return result;
+            }
         }
 
         interface IInstruction<T>
         {
             public IList<T> ApplyTechnique(IReadOnlyList<T> input);
+
+            public BigInteger IndexAfterTechniqueIsApplied(BigInteger index, long deckSize);
         }
     }
 }
